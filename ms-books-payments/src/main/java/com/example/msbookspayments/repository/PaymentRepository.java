11d1cb3704d897f7f129d1404c1efc09f98b0531
Miguel <migue@DESKTOP-08KN104.(none)>
2025-03-01 09:15:41 +0100

    payments

--- ms-books-payments/src/main/java/com/example/msbookspayments/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.msbookspayments.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.msbookspayments.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
